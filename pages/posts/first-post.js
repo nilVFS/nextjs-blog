aa693a8d2294076e3d9fdd48c6af9ccbc227488e
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2023-01-22 02:38:10 +0300

    complete assets/css/metadata section

---
+++ pages/posts/first-post.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </Layout>
+    )
+}
